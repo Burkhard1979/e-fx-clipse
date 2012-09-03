1cc45358dac63107b730ef1ac16afdddd501bc45
Tom Schindl <tom.schindl@bestsolution.at>
2012-09-03 17:31:42 +0200

    adding files for tycho build

---
+++ at.bestsolution.efxclipse.tooling.css.cssext/xtend-gen/at/bestsolution/efxclipse/tooling/css/cssext/generator/CssExtDslGenerator.java
@@ -0,0 +1,11 @@
+package at.bestsolution.efxclipse.tooling.css.cssext.generator;
+
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.xtext.generator.IFileSystemAccess;
+import org.eclipse.xtext.generator.IGenerator;
+
+@SuppressWarnings("all")
+public class CssExtDslGenerator implements IGenerator {
+  public void doGenerate(final Resource resource, final IFileSystemAccess fsa) {
+  }
+}
