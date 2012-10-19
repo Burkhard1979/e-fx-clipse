fbc6b3e75aa09748452753be6b14d3fe42b78302
Tom Schindl <tom.schindl@bestsolution.at>
2012-10-20 01:23:40 +0200

    fixed type of toolitem

--- at.bestsolution.efxclipse.runtime.workbench.renderers.base/src/at/bestsolution/efxclipse/runtime/workbench/renderers/base/widget/WToolItem.java
+++ at.bestsolution.efxclipse.runtime.workbench.renderers.base/src/at/bestsolution/efxclipse/runtime/workbench/renderers/base/widget/WToolItem.java
@@ -13,7 +13,7 @@ package at.bestsolution.efxclipse.runtime.workbench.renderers.base.widget;
 import org.eclipse.e4.ui.model.application.ui.menu.MToolItem;
 
 @SuppressWarnings("restriction")
-public interface WToolItem<N> extends WLayoutedWidget<MToolItem> {
+public interface WToolItem<N> extends WWidget<MToolItem> {
 	public void setOnActionCallback(Runnable onActionCallback);
 
 	public void setHandled(boolean handled);
