b72cffa24e0481d0940447d3d4e6d4ba55f6c516
Torsten Sommer <torsten.sommer@tesis.de>
2012-11-24 21:28:20 +0100

    Old ListView and DetailsView removed.

--- at.bestsolution.efxclipse.runtime.demo.contacts.app/src/at/bestsolution/efxclipse/runtime/demo/contacts/views/ListView.java
+++ at.bestsolution.efxclipse.runtime.demo.contacts.app/src/at/bestsolution/efxclipse/runtime/demo/contacts/views/ListView.java
@@ -12,58 +12,65 @@
 
 package at.bestsolution.efxclipse.runtime.demo.contacts.views;
 
-import at.bestsolution.efxclipse.runtime.demo.contacts.model.ContactsManager;
-
 import at.bestsolution.efxclipse.runtime.demo.contacts.Contact;
-
-import at.bestsolution.efxclipse.runtime.emf.edit.ui.AdapterFactoryObservableList;
-import at.bestsolution.efxclipse.runtime.emf.edit.ui.AdapterFactoryTableCellFactory;
-import at.bestsolution.efxclipse.runtime.emf.edit.ui.ProxyCellValueFactory;
+import at.bestsolution.efxclipse.runtime.demo.contacts.ContactsPackage;
+import at.bestsolution.efxclipse.runtime.demo.contacts.model.ContactsManager;
+import at.bestsolution.efxclipse.runtime.emf.edit.ui.AdapterFactoryTreeCellFactory;
+import at.bestsolution.efxclipse.runtime.emf.edit.ui.AdapterFactoryTreeItem;
+import at.bestsolution.efxclipse.runtime.emf.edit.ui.EAttributeCellEditHandler;
 import at.bestsolution.efxclipse.runtime.emf.edit.ui.dnd.CellDragAdapter;
 import at.bestsolution.efxclipse.runtime.emf.edit.ui.dnd.EditingDomainCellDropAdapter;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.TreeView;
 import javafx.scene.layout.BorderPane;
 import javax.inject.Inject;
 import org.eclipse.e4.ui.model.application.MApplication;
+import org.eclipse.emf.edit.domain.EditingDomain;
 
 @SuppressWarnings("restriction")
 public class ListView {
-	
+
 	@Inject
-	@SuppressWarnings("unchecked")
-	public ListView(BorderPane parent, final MApplication application, ContactsManager contactsManager) {
-		TableView<Contact> tableView = new TableView<>();
-		TableColumn<Contact, String> firstNameColumn = new TableColumn<>("First Name");
-		firstNameColumn.setCellValueFactory(new ProxyCellValueFactory<Contact, String>());
-		AdapterFactoryTableCellFactory<Contact, String> firstNameCellFactory = new AdapterFactoryTableCellFactory<>(contactsManager.getAdapterFactory(), 0);
-		firstNameCellFactory.addCellCreationListener(new CellDragAdapter());
-		firstNameCellFactory.addCellCreationListener(new EditingDomainCellDropAdapter(contactsManager.getEditingDomain()));
-		firstNameColumn.setCellFactory(firstNameCellFactory);
-		firstNameColumn.setSortable(false);
+	public ListView(BorderPane parent, final MApplication application, final ContactsManager contactsManager) {
+		EditingDomain editingDomain = contactsManager.getEditingDomain();
 
-		TableColumn<Contact, String> lastNameColumn = new TableColumn<>("Last Name");
-		lastNameColumn.setCellValueFactory(new ProxyCellValueFactory<Contact, String>());
-		AdapterFactoryTableCellFactory<Contact, String> lastNameCellFactory = new AdapterFactoryTableCellFactory<>(contactsManager.getAdapterFactory(), 1);
-		lastNameCellFactory.addCellCreationListener(new CellDragAdapter());
-		lastNameCellFactory.addCellCreationListener(new EditingDomainCellDropAdapter(contactsManager.getEditingDomain()));
-		lastNameColumn.setCellFactory(lastNameCellFactory);
-		lastNameColumn.setSortable(false);
-		
-		parent.setCenter(tableView);
+		// TreeView
+		TreeView<Object> treeView = new TreeView<>();
+		treeView.setRoot(new AdapterFactoryTreeItem(contactsManager.getRootGroup(), contactsManager.getAdapterFactory()));
+		AdapterFactoryTreeCellFactory treeCellFactory = new AdapterFactoryTreeCellFactory(contactsManager.getAdapterFactory());
 
-		tableView.getColumns().addAll(firstNameColumn, lastNameColumn);
-		
-		tableView.setItems(new AdapterFactoryObservableList<Contact>(contactsManager.getAdapterFactory(), contactsManager.getRootGroup()));
+		// add edit support
+		treeCellFactory.addCellEditHandler(new EAttributeCellEditHandler(ContactsPackage.eINSTANCE.getGroup_Name(), editingDomain));
 
-		tableView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Object>() {
+		// adds drag support
+		treeCellFactory.addCellCreationListener(new CellDragAdapter());
+
+		// adds drop support
+		treeCellFactory.addCellCreationListener(new EditingDomainCellDropAdapter(editingDomain));
+
+		treeView.setCellFactory(treeCellFactory);
+
+		parent.setCenter(treeView);
+
+		treeView.setShowRoot(false);
+
+		treeView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Object>() {
 
 			public void changed(ObservableValue<? extends Object> arg0, Object arg1, Object arg2) {
-				application.getContext().set(Contact.class, (Contact)arg2);
+				if (arg2 instanceof AdapterFactoryTreeItem) {
+					Object value = ((AdapterFactoryTreeItem) arg2).getValue();
+					if (value instanceof Contact)
+						application.getContext().set(Contact.class, (Contact) value);
+				}
 			}
-			
+
 		});
+
+		// add the context menu
+		ContextMenuProvider contextMenuProvider = new ContextMenuProvider(editingDomain);
+		treeCellFactory.addCellUpdateListener(contextMenuProvider);
+
+		treeView.setEditable(true);
 	}
 }
