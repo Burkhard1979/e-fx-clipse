94f7cf59c9f17328107a6ca2763608eb4b16fd33
Christoph Caks <ccaks@bestsolution.at>
2012-12-10 15:07:27 +0100

    logger improvements

--- at.bestsolution.efxclipse.runtime.core/src/at/bestsolution/efxclipse/runtime/core/log/Logger.java
+++ at.bestsolution.efxclipse.runtime.core/src/at/bestsolution/efxclipse/runtime/core/log/Logger.java
@@ -11,19 +11,40 @@
 package at.bestsolution.efxclipse.runtime.core.log;
 
 public interface Logger {
-	public void log(Level level, String message, Throwable... t);
 	
-	public void trace(String message, Throwable... t);
+	public void log(Level level, String message);
+	public void log(Level level, String message, Throwable t);
 	
-	public void debug(String message, Throwable... t);
+	public void logf(Level level, String pattern, Object...args);
+	public void logf(Level level, String pattern, Throwable t, Object...args);
 	
-	public void info(String message, Throwable... t);
+	public void trace(String message);
+	public void debug(String message);
+	public void info(String message);
+	public void warning(String message);
+	public void error(String message);
+	public void fatal(String message);
 	
-	public void warning(String message, Throwable... t);
+	public void trace(String message, Throwable t);
+	public void debug(String message, Throwable t);
+	public void info(String message, Throwable t);
+	public void warning(String message, Throwable t);
+	public void error(String message, Throwable t);
+	public void fatal(String message, Throwable t);
 	
-	public void error(String message, Throwable... t);
+	public void tracef(String pattern, Object...args);
+	public void debugf(String pattern, Object...args);
+	public void infof(String pattern, Object...args);
+	public void warningf(String pattern, Object...args);
+	public void errorf(String pattern, Object...args);
+	public void fatalf(String pattern, Object...args);
 	
-	public void fatal(String message, Throwable... t);
+	public void tracef(String pattern, Throwable t, Object...args);
+	public void debugf(String pattern, Throwable t, Object...args);
+	public void infof(String pattern, Throwable t, Object...args);
+	public void warningf(String pattern, Throwable t, Object...args);
+	public void errorf(String pattern, Throwable t, Object...args);
+	public void fatalf(String pattern, Throwable t, Object...args);
 	
 	public boolean isEnabled(Level level);
 	
