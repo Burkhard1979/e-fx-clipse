bb52cc44a24c1c40e7274078eee1940e21cc48ca
Tom Schindl <tom.schindl@bestsolution.at>
2012-12-11 16:31:27 +0100

    moving main preference page to root tooling bundle

---
+++ at.bestsolution.efxclipse.tooling.ui/src/at/bestsolution/efxclipse/tooling/ui/wizards/internal/JFXWorkbenchPreferencePage.java
@@ -0,0 +1,34 @@
+package at.bestsolution.efxclipse.tooling.ui.wizards.internal;
+
+import org.eclipse.jface.preference.PreferencePage;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+public class JFXWorkbenchPreferencePage extends PreferencePage implements IWorkbenchPreferencePage {
+
+	public JFXWorkbenchPreferencePage() {
+	}
+
+	public JFXWorkbenchPreferencePage(String title) {
+		super(title);
+	}
+
+	public JFXWorkbenchPreferencePage(String title, ImageDescriptor image) {
+		super(title, image);
+	}
+
+	@Override
+	public void init(IWorkbench workbench) {
+
+	}
+
+	@Override
+	protected Control createContents(Composite parent) {
+		return new Composite(parent, SWT.NONE);
+	}
+
+}
