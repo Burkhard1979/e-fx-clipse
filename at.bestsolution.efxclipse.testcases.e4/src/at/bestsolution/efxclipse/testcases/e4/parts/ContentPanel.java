b65a2db87a95f83051c00a29c1692ba989fe57d7
Tom Schindl <tom.schindl@bestsolution.at>
2012-12-13 16:19:58 +0100

    adding event sample

--- at.bestsolution.efxclipse.testcases.e4/src/at/bestsolution/efxclipse/testcases/e4/parts/ContentPanel.java
+++ at.bestsolution.efxclipse.testcases.e4/src/at/bestsolution/efxclipse/testcases/e4/parts/ContentPanel.java
@@ -1,9 +1,16 @@
 package at.bestsolution.efxclipse.testcases.e4.parts;
 
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
+import org.eclipse.e4.core.di.annotations.Optional;
+import org.eclipse.e4.core.di.extensions.EventTopic;
 import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.e4.ui.di.UIEventTopic;
+import org.eclipse.e4.ui.di.UISynchronize;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
+
+import at.bestsolution.efxclipse.testcases.e4.handlers.EventSender;
 
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
@@ -12,6 +19,9 @@ import javafx.scene.layout.BorderPane;
 public class ContentPanel {
 	
 	private TextField f;
+	
+	@Inject
+	protected UISynchronize uiSync;
 
 	@Inject
 	public ContentPanel(BorderPane p, MPart part) {
@@ -19,8 +29,37 @@ public class ContentPanel {
 		p.setTop(f);
 	}
 	
+	@PostConstruct
+	void sync() {
+		System.err.println("The sync: " + uiSync);
+	}
+	
 	@Focus
 	void focus() {
 		f.requestFocus();
+	}
+	
+//	@Inject
+//	@Optional
+//	void eventSync(@EventTopic(EventSender.SYNC_EVENT) String event) {
+//		System.err.println("SYNC: Received event: " + event + " on thread " + Thread.currentThread());
+//	}
+//	
+//	@Inject
+//	@Optional
+//	void eventASync(@EventTopic(EventSender.ASYNC_EVENT) String event) {
+//		System.err.println("ASYNC: Received event: " + event + " on thread " + Thread.currentThread());
+//	}
+	
+	@Inject
+	@Optional
+	void eventSyncUI(@UIEventTopic(EventSender.SYNC_EVENT) String event) {
+		System.err.println("UI-SYNC: Received event: " + event + " on thread " + Thread.currentThread());
+	}
+	
+	@Inject
+	@Optional
+	void eventASyncUI(@UIEventTopic(EventSender.ASYNC_EVENT) String event) {
+		System.err.println("UI-ASYNC: Received event: " + event + " on thread " + Thread.currentThread());
 	}
 }
