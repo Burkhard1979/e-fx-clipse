0b0d32c074748b50427c59890e13e34c1c0295cb
Tom Schindl <tom.schindl@bestsolution.at>
2012-12-28 11:39:18 +0100

    switching to task robot

--- at.bestsolution.efxclipse.tooling.pde.e4/src/at/bestsolution/efxclipse/tooling/pde/e4/project/media/NewMediaProjectWizard.java
+++ at.bestsolution.efxclipse.tooling.pde.e4/src/at/bestsolution/efxclipse/tooling/pde/e4/project/media/NewMediaProjectWizard.java
@@ -10,22 +10,93 @@
  *******************************************************************************/
 package at.bestsolution.efxclipse.tooling.pde.e4.project.media;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.actions.WorkspaceModifyOperation;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceReference;
+
+import at.bestsolution.efxclipse.tooling.pde.ui.wizard.BundleConfigurationPage;
+import at.bestsolution.efxclipse.tooling.pde.ui.wizard.model.BundleProjectData;
+import at.bestsolution.efxclipse.tooling.rrobot.RRobot;
+import at.bestsolution.efxclipse.tooling.rrobot.dsl.FileLoader;
+import at.bestsolution.efxclipse.tooling.rrobot.model.task.RobotTask;
 
 public class NewMediaProjectWizard extends Wizard implements INewWizard {
-
+	private BundleProjectData data;
+	
+	public NewMediaProjectWizard() {
+		this.data = new BundleProjectData();
+		this.data.setVersion("1.0.0.qualifier");
+	}
+	
 	@Override
 	public void init(IWorkbench workbench, IStructuredSelection selection) {
 		
 	}
 
 	@Override
+	public void addPages() {
+		super.addPages();
+		addPage(new BundleConfigurationPage(data,"bundle.page","New e4 FX application") {
+			@Override
+			protected boolean withBundleDescription() {
+				return false;
+			}
+			
+			@Override
+			protected boolean withBundleVersion() {
+				return false;
+			}
+		});
+	}
+	
+	@Override
 	public boolean performFinish() {
-		// TODO Auto-generated method stub
-		return false;
+		Bundle b = FrameworkUtil.getBundle(getClass());
+		BundleContext ctx = b.getBundleContext();
+		ServiceReference<RRobot> ref = ctx.getServiceReference(RRobot.class);
+		final RRobot r = ctx.getService(ref);
+
+		FileLoader loader = FileLoader.createLoader();
+		final RobotTask task = loader.loadTask(URI.createPlatformPluginURI("/at.bestsolution.efxclipse.tooling.pde.e4/generator-tasks/mediaSample.rtask", true));
+
+		final Map<String,Object> additionalData = new HashMap<>();
+		additionalData.put("BundleProject_projectName", data.getSymbolicname());
+		additionalData.put("BundleProject_bundleId", data.getSymbolicname());
+		additionalData.put("BundleProject_bundleVendor", data.getVendor() != null ? data.getVendor() : "");
+//		additionalData.put("BundleProject_bundleVersion", data.getVersion());
+		additionalData.put("BundleProject_EE", data.getEEnv());
+		
+		WorkspaceModifyOperation w = new WorkspaceModifyOperation() {
+			
+			@Override
+			protected void execute(IProgressMonitor monitor) throws CoreException, InvocationTargetException, InterruptedException {
+				r.executeTask(monitor, task, additionalData);
+			}
+		};
+		
+		try {
+			getContainer().run(true, true,w);
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return true;
 	}
 
 }
