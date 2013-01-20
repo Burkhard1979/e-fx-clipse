ddf1d9fea825ed25cfd2275d7864a728ca0942e0
Tom Schindl <tom.schindl@bestsolution.at>
2013-01-20 14:13:20 +0100

    #287 - Import errors when starting Eclipse with Java 1.6 runtime

--- at.bestsolution.efxclipse.tooling.java6/src/at/bestsolution/efxclipse/tooling/java6/JavaVersionCheck.java
+++ at.bestsolution.efxclipse.tooling.java6/src/at/bestsolution/efxclipse/tooling/java6/JavaVersionCheck.java
@@ -10,12 +10,12 @@ public class JavaVersionCheck implements IStartup {
 
 	@Override
 	public void earlyStartup() {
-		Version minVersion = new Version("1.8.0");
+		Version minVersion = new Version("1.7.0");
 		Version realVersion = new Version(System.getProperty("org.osgi.framework.version")); 
 		
 		if(minVersion.compareTo(realVersion) == 1 ) {
 			StatusManager.getManager().handle(
-					new Status(IStatus.ERROR, "at.bestsolution.efxclipse.tooling.java6", "You are not running your eclipse instance with Java7 or Java8. The JavaFX tooling is disabled because of this."),StatusManager.SHOW);
+					new Status(IStatus.ERROR, "at.bestsolution.efxclipse.tooling.java6", "You are not running your eclipse instance with Java7 or Java8. The JavaFX tooling is disabled because of this.\r\nMake Java7 the default java or adjust your eclipse.ini"),StatusManager.SHOW);
 		}
 	}
 
