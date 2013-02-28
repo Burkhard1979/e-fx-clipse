1c97f333aea4f9e7e153ed596c44df9cadae9654
Tom Schindl <tom.schindl@bestsolution.at>
2013-03-01 00:07:11 +0100

    refactored toolbar/menu support

--- at.bestsolution.efxclipse.runtime.workbench.renderers.fx/src/at/bestsolution/efxclipse/runtime/workbench/renderers/fx/DefPartRenderer.java
+++ at.bestsolution.efxclipse.runtime.workbench.renderers.fx/src/at/bestsolution/efxclipse/runtime/workbench/renderers/fx/DefPartRenderer.java
@@ -11,10 +11,12 @@
 package at.bestsolution.efxclipse.runtime.workbench.renderers.fx;
 
 import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 
 import javax.inject.Inject;
 
@@ -31,37 +33,41 @@ import at.bestsolution.efxclipse.runtime.workbench.renderers.base.widget.WToolBa
 import at.bestsolution.efxclipse.runtime.workbench.renderers.fx.widget.WLayoutedWidgetImpl;
 
 @SuppressWarnings("restriction")
-public class DefPartRenderer extends BasePartRenderer<BorderPane,Node,Node> {
+public class DefPartRenderer extends BasePartRenderer<BorderPane, Node, Node> {
 	private static final String TOOLBAR_MENU_FLOAT_TAG = "Part-ToolBarMenu-Floating";
+	private static final String TOOLBAR_MENU_COLLAPSIBLE_TAG = "Part-ToolBarMenu-Collapsible";
 	private static final String TOOLBAR_MENU_BOTTOM_TAG = "Part-ToolBarMenu-Bottom";
-	
+
 	private static final String TOOL_BAR_FULL_SPAN_TAG = "Part-Toolbar-FullSpan";
-	
+
 	@Override
-	protected Class<? extends WPart<BorderPane,Node,Node>> getWidgetClass(MPart part) {
+	protected Class<? extends WPart<BorderPane, Node, Node>> getWidgetClass(MPart part) {
 		return PartImpl.class;
 	}
 
 	@Override
-	protected boolean requiresFocus(WPart<BorderPane,Node,Node> widget) { 
+	protected boolean requiresFocus(WPart<BorderPane, Node, Node> widget) {
 		Node n = (Node) widget.getWidget();
-		
+
 		do {
-			if( n.getUserData() == widget ) {
+			if (n.getUserData() == widget) {
 				return false;
 			}
-		} while( (n = n.getParent()) != null );
-		
+		} while ((n = n.getParent()) != null);
+
 		return true;
 	}
-	
-	public static class PartImpl extends WLayoutedWidgetImpl<BorderPane, AnchorPane, MPart> implements WPart<BorderPane,Node,Node> {
+
+	public static class PartImpl extends WLayoutedWidgetImpl<BorderPane, AnchorPane, MPart> implements WPart<BorderPane, Node, Node> {
 		@Inject
 		EPartService service;
-		
+
 		private AnchorPane contentArea;
 		private BorderPane dataArea;
 		private BorderPane toolbarMenuContainer;
+
+		private StackPane toolbarGroup;
+		private Group menuGroup;
 		
 		@Override
 		protected BorderPane createWidget() {
@@ -71,116 +77,128 @@ public class DefPartRenderer extends BasePartRenderer<BorderPane,Node,Node> {
 				@Override
 				public void handle(MouseEvent event) {
 					event.consume();
-					service.activate(getDomElement(),true);
-					if( !checkFocusControl() ) {
-						ContextInjectionFactory.invoke(getDomElement().getObject(), Focus.class, getDomElement().getContext(),null);
-						if( !checkFocusControl() ) {
-							p.requestFocus();	
+					service.activate(getDomElement(), true);
+					if (!checkFocusControl()) {
+						ContextInjectionFactory.invoke(getDomElement().getObject(), Focus.class, getDomElement().getContext(), null);
+						if (!checkFocusControl()) {
+							p.requestFocus();
 						}
 					}
 				}
 			});
 			return p;
 		}
-		
+
 		private boolean checkFocusControl() {
 			BorderPane check = getWidget();
-			if( check == null || check.getScene() == null ) {
+			if (check == null || check.getScene() == null) {
 				return false;
 			}
-			
+
 			Node n = check.getScene().getFocusOwner();
-			
-			if( n == null ) {
+
+			if (n == null) {
 				return false;
 			}
-			
+
 			while (n.getParent() != null) {
 				if (n.getParent() == check) {
 					return true;
 				}
 				n = n.getParent();
-				
+
 			}
 			return false;
 		}
 
 		@Override
 		protected AnchorPane getWidgetNode() {
-			if( contentArea == null ) {
-				contentArea = new AnchorPane();
+			if (contentArea == null) {
+				menuGroup = new Group();
+				menuGroup.setVisible(false);
+				menuGroup.setManaged(false);
+				contentArea = new AnchorPane() {
+					@Override
+					protected void layoutChildren() {
+						super.layoutChildren();
+						if( menuGroup.isVisible() ) {
+							menuGroup.relocate(contentArea.getWidth()-20,0);
+						}
+					}
+				};
 				dataArea = new BorderPane();
-				
+
 				AnchorPane.setTopAnchor(dataArea, 0.0);
 				AnchorPane.setLeftAnchor(dataArea, 0.0);
 				AnchorPane.setBottomAnchor(dataArea, 1.0);
 				AnchorPane.setRightAnchor(dataArea, 1.0);
-				
-				contentArea.getChildren().add(dataArea);
-				
+
+				contentArea.getChildren().addAll(dataArea, menuGroup);
 				Node n = getWidget();
-				if( n != null ) {
+				if (n != null) {
 					n.getStyleClass().add("part-content");
 					dataArea.setCenter(n);
 				} else {
 					logger.log(Level.ERROR, "No widget node to attach");
-				}				
+				}
 			}
 			return contentArea;
 		}
 
 		private void initToolbarMenu() {
-			if( toolbarMenuContainer == null ) {
+			if (toolbarMenuContainer == null) {
 				// Ensure that everything is initialized!!!
 				getStaticLayoutNode();
-				BorderPane p = new BorderPane();
-				p.getStyleClass().add("view-toolbar-menu");
-				toolbarMenuContainer = new BorderPane();
-				if( getDomElement().getTags().contains(TOOL_BAR_FULL_SPAN_TAG) ) {
-					p.setCenter(toolbarMenuContainer);
-				} else {
-					p.setRight(toolbarMenuContainer);	
-				}
 				
-				if( getDomElement().getTags().contains(TOOLBAR_MENU_BOTTOM_TAG) ) {
-					dataArea.setBottom(p);
+				toolbarGroup = new StackPane();
+				
+				if (getDomElement().getTags().contains(TOOL_BAR_FULL_SPAN_TAG)) {
+					BorderPane p = new BorderPane();
+					p.setCenter(toolbarGroup);
+					p.getStyleClass().add("view-toolbar-container");
+					if (getDomElement().getTags().contains(TOOLBAR_MENU_BOTTOM_TAG)) {
+						dataArea.setBottom(p);
+					} else {
+						dataArea.setTop(p);	
+					}
 				} else {
-					dataArea.setTop(p);	
+					BorderPane p = new BorderPane();
+					p.setRight(toolbarGroup);
+					p.getStyleClass().add("view-toolbar-container");
+					if (getDomElement().getTags().contains(TOOLBAR_MENU_BOTTOM_TAG)) {
+						dataArea.setBottom(p);
+					} else {
+						dataArea.setTop(p);	
+					}
 				}
 			}
 		}
-		
-		private void checkToolbarMenu() {
-			if( toolbarMenuContainer.getLeft() == null && toolbarMenuContainer.getRight() == null ) {
-				toolbarMenuContainer = null;
-				dataArea.setTop(null);
-			}
-		}
-		
+
 		@Override
 		public void setToolbar(WToolBar<Node> widget) {
-			if( widget == null ) {
-				if( toolbarMenuContainer != null ) {
-					toolbarMenuContainer.setLeft(null);
-					checkToolbarMenu();
+			if (widget == null) {
+				if (toolbarGroup != null) {
+					toolbarGroup.getChildren().clear();
+					dataArea.setTop(null);
+					dataArea.setBottom(null);
 				}
 			} else {
 				initToolbarMenu();
-				((Node)widget.getWidget()).getStyleClass().add("view-toolbar");
-				toolbarMenuContainer.setLeft((Node) widget.getStaticLayoutNode());
+				((Node) widget.getWidget()).getStyleClass().add("view-toolbar");
+				toolbarGroup.getChildren().setAll((Node) widget.getWidget());
 			}
 		}
-		
+
 		@Override
 		public void setMenu(WMenu<Node> widget) {
-			if( widget == null ) {
-				if( toolbarMenuContainer != null ) {
-					toolbarMenuContainer.setRight(null);
-					checkToolbarMenu();
+			if (widget == null) {
+				if (menuGroup != null) {
+					menuGroup.setVisible(false);
+					menuGroup.getChildren().clear();
 				}
 			} else {
-				initToolbarMenu();
-				toolbarMenuContainer.setRight((Node) widget.getWidget());
+				menuGroup.setVisible(true);
+				menuGroup.getChildren().setAll((Node) widget.getWidget());
 			}
 		}
 	}
